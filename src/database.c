90ab2ea7e571d57158f47af223fddd9ff51bfb7b
kmeow <kmeow@meow.meow>
2020-01-25 04:49:18 -0600

    Fix makefile for sqlite

--- src/database.c
+++ src/database.c
@@ -1,12 +1,14 @@
 #include "database.h"
+#include "sqlite/sqlite3.h"
 
-int Database_Init(struct Database* asdb)
+int Database_Init(struct Database* asdb, const char* path)
 {
+	int result = sqlite3_open(path, &asdb->ppdb);
 
 	return 0;
 }
 
 void Database_Destroy(struct Database* asdb)
 {
-
+	sqlite3_close(asdb->ppdb);
 }
--- src/database.h
+++ src/database.h
@@ -4,10 +4,11 @@
 
 struct Database
 {
-
+	sqlite3* ppdb;
+	char path[256];
 };
 
-int Database_Init(struct ASDatabase* asdb);
+int Database_Init(struct ASDatabase* asdb, const char* path);
 void Database_Destroy(struct ASDatabase* asdb);
 
 #endif
