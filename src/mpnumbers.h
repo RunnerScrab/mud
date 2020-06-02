84b0862d2e957dc6be8ddfb502e4c55b47daa9fd
kmeow <kmeow@meow.meow>
2020-06-02 18:52:05 -0500

    Break MPInt out into separate source files

--- src/mpnumbers.h
+++ src/mpnumbers.h
@@ -11,12 +11,12 @@
 #include "utils.h"
 #include "gmp/gmp.h"
 
-
 /*
   Angelscript bindings to gmp multiprecision library. We bind
   the C interface directly, as Angelscript objects need special
   handling for reference counting
 */
+
 #ifdef __cplusplus
 class asIScriptEngine;
 class asILockableSharedBool;
@@ -49,145 +49,8 @@ private:
 	AllocPool m_allocpool;
 };
 
-class MPInt:
-	public AS_RefCountedObj, public RWLockingObject
-{
-	friend MPInt* operator-(const unsigned int a, const MPInt& mpnum);
-public:
-	static MemoryPoolAllocator* m_static_mempool;
-
-	template <typename T> static MPInt* Factory(const T initvalue)
-	{
-		if(m_static_mempool)
-		{
-			void* pMem = m_static_mempool->Alloc();
-			return new(pMem) MPInt(initvalue);
-		}
-		else
-		{
-			return 0;
-		}
-	}
-
-	static MPInt* Factory(const MPInt& initvalue)
-	{
-		if(m_static_mempool)
-		{
-			void* pMem = m_static_mempool->Alloc();
-			return new(pMem) MPInt(initvalue);
-		}
-		else
-		{
-			return 0;
-		}
-	}
-
-	void operator delete(void* p)
-	{
-		if(m_static_mempool)
-		{
-			m_static_mempool->Free(p);
-		}
-	}
-
-	MPInt(const MPInt& other)
-	{
-		other.ReadLock();
-		WriteLock();
-		mpz_init_set(m_value, other.m_value);
-		Unlock();
-		other.Unlock();
-	}
-
-	MPInt(const int initval = 0)
-	{
-		WriteLock();
-		mpz_init_set_si(m_value, initval);
-		Unlock();
-	}
-
-	MPInt(const unsigned int initval)
-	{
-		WriteLock();
-		mpz_init_set_ui(m_value, initval);
-		Unlock();
-	}
-
-	MPInt(const double initval)
-	{
-		WriteLock();
-		mpz_init_set_d(m_value, initval);
-		Unlock();
-	}
-
-	~MPInt()
-	{
-		mpz_clear(m_value);
-	}
-
-	//Assignment operators
-	MPInt& operator=(const MPInt& other);
-	MPInt& operator=(const unsigned int num);
-	MPInt& operator=(const int num);
-	MPInt& operator=(const double num);
-
-	//Assignment-arithmetic
-	MPInt& operator+=(const MPInt& other);
-	MPInt& operator+=(const unsigned int num);
-	MPInt& operator-=(const MPInt& other);
-	MPInt& operator-=(const unsigned int num);
-	MPInt& operator*=(const MPInt& other);
-	MPInt& operator*=(const unsigned int num);
-	MPInt& operator*=(const int num);
-	MPInt& operator/=(const MPInt& other);
-	MPInt& operator/=(const unsigned int num);
-	MPInt& operator%=(const MPInt& other);
-	MPInt& operator%=(const unsigned int);
-	MPInt& powassign(const unsigned int power);
-	//Comparison operators
-	bool operator==(const MPInt &other) const;
-	bool operator==(const double num) const;
-	bool operator==(const int num) const;
-	bool operator==(const unsigned int num) const;
-
-	int opCmp(const MPInt& other) const;
-	int opCmp(const double num) const;
-	int opCmp(const int num) const;
-	int opCmp(const unsigned int num) const;
-
-	bool isSame(const MPInt& other) const; //Compares object references
-	bool isNotSame(const MPInt& other) const;
-
-	//Arithmetic operators
-	MPInt* operator+(const MPInt& other);
-	MPInt* operator+(const unsigned int num);
-
-	MPInt* operator-();
-	MPInt* operator-(const MPInt& other);
-	MPInt* operator-(const unsigned int num);
-
-	MPInt* operator*(const MPInt& other);
-	MPInt* operator*(const unsigned int num);
-	MPInt* operator*(const int num);
-
-	MPInt* operator/(const MPInt& other);
-	MPInt* operator/(const unsigned int num);
-
-	MPInt* operator%(const MPInt& other);
-	MPInt* operator%(const unsigned int);
-	MPInt* pow(const unsigned int power);
-	MPInt* Abs();
-
-	//Conversion operators
-	const std::string toString();
-private:
-	mpz_t m_value;
-};
-
-int RegisterMPIntClass(asIScriptEngine* engine);
 int RegisterMPNumberClasses(asIScriptEngine *engine);
-
-MPInt* operator-(const unsigned int a, const MPInt& mpnum);
+void SetASException(const char* msg);
 
 extern "C"
 {
